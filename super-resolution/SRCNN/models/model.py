ad4a62662a9bbe86cb61bd6b37751bbe7d133bcb
TianheWu <tianhe_wu@foxmail.com>
2021-10-31 00:55:20 +0800

    finish basic srcnn

--- super-resolution/SRCNN/models/model.py
+++ super-resolution/SRCNN/models/model.py
@@ -4,9 +4,10 @@ from torch import nn
 class SRCNN(nn.Module):
 
     def __init__(self, in_channels):
-        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=9)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=1)
-        self.conv3 = nn.Conv2d(32, 1, kernel_size=5)
+        super(SRCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(32, 1, kernel_size=5, padding=2)
         self.relu = nn.ReLU(inplace=True)
     
     def forward(self, x):
--- super-resolution/SRCNN/utils/data/Datasets.py
+++ super-resolution/SRCNN/utils/data/Datasets.py
@@ -4,17 +4,22 @@ import cv2
 import os
 import copy
 import skimage
+import numpy as np
 
 from skimage import img_as_float, img_as_ubyte
 
 
 class ImageSet(torch.utils.data.Dataset):
-    def __init__(self, img):
+    def __init__(self, img, start=0.0, end=1.0):
         """
         :param img: The file path of datasets.
         """
         super(ImageSet, self).__init__()
-        self.imgs = [os.path.join(img, file) for file in os.listdir(img) if ImageSet.is_img(file)]
+        self.imgs = [os.path.join(img, file).replace('\\', '/') for file in os.listdir(img) if ImageSet.is_img(file)]
+        length = len(self.imgs)
+        if length == 0:
+            raise FileNotFoundError('No dataset files found. Check path: {}'.format(img))
+        self.imgs = self.imgs[int(length * start):int(length * end)]
     
     @staticmethod
     def open2sk(img):
@@ -46,22 +51,22 @@ class ImageSet(torch.utils.data.Dataset):
         :param img: Image file path.
         :return: Wheter is is an image, type boolean.
         """
-        return any([img.endswidth(extension) for extension in [".jpg", ".png", ".jpeg"]])
+        return any([img.endswith(extension) for extension in [".jpg", ".png", ".jpeg"]])
 
     def __getitem__(self, index):
         """
-        :return: The noise image and label
+        :return: The noise image and label.
         """
-        image = cv2.imread(self.imgs[index])
-        skimg = ImageSet.open2sk(image)
+        origin_image = cv2.imread(self.imgs[index])
+        skimg = ImageSet.open2sk(origin_image)
         noise_skimg = skimage.util.random_noise(skimg, mode='gaussian')
         noise_opimg = ImageSet.sk2open(noise_skimg)
-        noise_opimg = ImageSet.bgr2ycbcr(noise_opimg)
-        label = copy.deepcopy(image)
-        return noise_opimg, label
+        data = torch.from_numpy(ImageSet.bgr2ycbcr(noise_opimg)[:, :, 0].astype(np.float32)).unsqueeze(0) / 255
+        label = torch.from_numpy(ImageSet.bgr2ycbcr(origin_image)[:, :, 0].astype(np.float32)).unsqueeze(0) / 255
+        return data, label
 
     def __len__(self):
         """
-        :return: The length of datasets
+        :return: The length of datasets.
         """
         return len(self.imgs)
