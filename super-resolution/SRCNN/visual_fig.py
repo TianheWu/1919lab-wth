22e25ce2113b5b5ba67ade6f6357cf6041b67efe
TianheWu <tianhe_wu@foxmail.com>
2021-11-01 01:37:45 +0800

    update weekwork13

--- super-resolution/SRCNN/visual_fig.py
+++ super-resolution/SRCNN/visual_fig.py
@@ -16,12 +16,13 @@ def visual(img):
     cv2.imwrite('bicubic.jpg', origin_image)
     image_channel = ImageSet.extract_channel(origin_image, channel='ycrcb')
     net = SRCNN(3)
-    model_path = "output/epoch149_loss_0.0465_statedict.pt"
+    model_path = "output/epoch99_loss_0.0517_statedict.pt"
     net.load_state_dict(torch.load(model_path, map_location='cpu'), strict=False)
     net.eval()
     with torch.no_grad():
         pred_fig_channel = net(image_channel.unsqueeze(0))
     pred_fig_channel = pred_fig_channel.squeeze(0).detach().numpy() * 255
+    print("shape: ", pred_fig_channel.shape)
     ret_fig = ImageSet.instead_channel(origin_image, pred_fig_channel, channel='ycrcb')
     cv2.imwrite('srcnn.jpg', ret_fig)
 
